c9708ad7f143119554d047096dd1f3267a99cc6e
Archakov Zayd <archakov5664@gmail.com>
2025-12-16 22:54:35 +0300

    bb

---
+++ pr23-2/src/Queue.java
@@ -0,0 +1,10 @@
+package prac23;
+
+public interface Queue {
+    void enqueue(Object element);
+    Object element();
+    Object dequeue();
+    int size();
+    boolean isEmpty();
+    void clear();
+}
